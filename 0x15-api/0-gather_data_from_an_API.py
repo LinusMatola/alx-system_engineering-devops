58a261536c248f789b24b19c3aef746ff09a07fb
Linus Matola <49551391+LinusMatola@users.noreply.github.com>
2022-11-29 08:10:39 +0300

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -12,26 +12,26 @@ from sys import argv
 if __name__ == "__main__":
     response = get('https://jsonplaceholder.typicode.com/todos/')
     data = response.json()
-    completed = 0
-    total = 0
+    NUMBER_OF_DONE_TASKS = 0
+    TOTAL_NUMBER_OF_TASKS = 0
     tasks = []
     response2 = get('https://jsonplaceholder.typicode.com/users')
     data2 = response2.json()
 
     for i in data2:
         if i.get('id') == int(argv[1]):
-            employee = i.get('name')
+            EMPLOYEE_NAME = i.get('name')
 
     for i in data:
         if i.get('userId') == int(argv[1]):
-            total += 1
+            TOTAL_NUMBER_OF_TASKS += 1
 
             if i.get('completed') is True:
-                completed += 1
+                NUMBER_OF_DONE_TASKS += 1
                 tasks.append(i.get('title'))
 
-    print("Employee {} is done with tasks({}/{}):".format(employee, completed,
-                                                          total))
+    print("Employee {} is done with tasks({}/{}):".format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS,
+                                                          TOTAL_NUMBER_OF_TASKS))
 
     for i in tasks:
         print("\t {}".format(i))
